e9aeddff1b5d296b4f3e958d0b8eec1ee7eed0dd
Semion <sfurlender@gmail.com>
2023-01-30 10:18:52 +0200

    .

---
+++ MyIEnumerable/MyIEnumerable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyIEnumerable
+{
+    internal class MyIEnumerable : IEnumerable
+    {
+        MyIEnumerator i = new MyIEnumerator();
+        public void Add(int a)
+        {
+            i.Add(a);
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return i;
+        }
+    }
+}
---
+++ MyIEnumerable/MyIEnumerator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyIEnumerable
+{
+    internal class MyIEnumerator : IEnumerator
+    {
+        int[] arr = new int[100];
+        int idx = 0;
+        public void Add(int s)
+        {
+            arr[idx] = s;
+            idx++;
+        }
+        public int GetId()
+        {
+            return 90;
+        }
+        int loop = -1;
+        public object Current
+        {
+            get
+            {
+                return arr[loop];
+            }
+        }
+        public bool MoveNext()
+        {
+
+            if (loop >= idx)
+            {
+                //Reset();
+                return false;
+            }
+            else
+            {
+                loop++;
+                if (arr[loop] == 33)
+                {
+                    return MoveNext();
+                }
+                return true;
+            }
+
+        }
+
+        public void Reset()
+        {
+            loop = -1;
+        }
+    }
+}
